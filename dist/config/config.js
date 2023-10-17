d200fc4ff97a8025a0c94f4965c32fd185fe87f3
tizianaamica <tiziana.amicarella@unet.edu.ve>
2023-10-17 12:11:57 -0500

    db atalas config

--- dist/config/config.js
+++ dist/config/config.js
@@ -3,7 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     jwtSecret: process.env.JWT_SECRET || "somesecrettoken",
     DB: {
-        URI: process.env.MONGODB_URI || "mongodb://localhost/jwttutorial",
+        URI: process.env.MONGODB_URI ||
+            "mongodb+srv://tizianaamica:prueba123@cluster0.dfl4ehq.mongodb.net/?retryWrites=true&w=majority",
         USER: process.env.MONGODB_USER,
         PASSWORD: process.env.MONGODB_PASSWORD,
     },
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,9 @@
 export default {
   jwtSecret: process.env.JWT_SECRET || "somesecrettoken",
   DB: {
-    URI: process.env.MONGODB_URI || "mongodb://localhost/jwttutorial",
+    URI:
+      process.env.MONGODB_URI ||
+      "mongodb+srv://tizianaamica:prueba123@cluster0.dfl4ehq.mongodb.net/?retryWrites=true&w=majority",
     USER: process.env.MONGODB_USER,
     PASSWORD: process.env.MONGODB_PASSWORD,
   },
